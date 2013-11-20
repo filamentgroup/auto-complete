7681a6886f7e283fc3aef2564c62f994ee4b01d5
Scott Jehl <scottjehl@gmail.com>
2013-11-20 10:35:31 -0600

    Create component.js

---
+++ component.js
@@ -0,0 +1,24 @@
+/*
+ * component
+ * Copyright (c) 2013 Filament Group, Inc.
+ * Licensed under MIT
+ */
+
+(function( w, $ ){
+
+	var pluginName = "plugin-name-here",
+		enhancedAttr = "data-enhanced",
+		initSelector = "." + pluginName + ":not([" + enhancedAttr + "])";
+
+	$.fn[ pluginName ] = function( $input ){
+		return this.each( function(){
+			// make enhancements here
+		});
+	};
+
+	// auto-init on enhance (which is called on domready)
+	$( document ).bind( "enhance", function( e ){
+		$( initSelector, e.target )[ pluginName ]().attr( "data-enhanced", true );
+	});
+
+}( jQuery ));
