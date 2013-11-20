fed255bf672a5960867054b57ace139fdbf0e036
Scott Jehl <scottjehl@gmail.com>
2013-11-20 10:47:57 -0600

    Update component.js

--- component.js
+++ component.js
@@ -1,16 +1,16 @@
 /*
- * simple auto-enhance component skeleton
+ * simple auto-enhance-able component skeleton
  * Copyright (c) 2013 Filament Group, Inc.
  * Licensed under MIT
  */
 
 (function( $ ){
 
-	var pluginName = "plugin-name-here",
+	var componentName = "component-name-here",
 		enhancedAttr = "data-enhanced",
-		initSelector = "." + pluginName + ":not([" + enhancedAttr + "])";
+		initSelector = "." + componentName + ":not([" + enhancedAttr + "])";
 
-	$.fn[ pluginName ] = function( $input ){
+	$.fn[ componentName ] = function( $input ){
 		return this.each( function(){
 			// make enhancements here
 		});
@@ -18,7 +18,7 @@
 
 	// auto-init on enhance (which is called on domready)
 	$( document ).bind( "enhance", function( e ){
-		$( initSelector, e.target )[ pluginName ]().attr( "data-enhanced", true );
+		$( initSelector, e.target )[ componentName ]().attr( "data-enhanced", true );
 	});
 
 }( jQuery ));
